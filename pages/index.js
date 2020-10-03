432bcb7d6895e9c4be078897a78978793c86640e
ShankarAswal <shankaraswal@gmail.com>
2020-10-03 13:26:15 +0100

    updated product list ui

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,52 @@
 import Link from "next/link";
 const Home = ({ products }) => {
   console.log(products);
+
+  const productList = products.map(product => (
+    <div className='col s4' key={product._id}>
+      <div className='card'>
+        <div className='card-image waves-effect waves-block waves-light'>
+          <img className='activator' src={product.mediaUrl} />
+        </div>
+        <div className='card-content'>
+          <span className='card-title activator grey-text text-darken-4'>
+            {product.name}
+            <i className='material-icons right'>more_vert</i>
+          </span>
+          <h6 className='text-red bold'>INR: {product.price}.00</h6>
+          <p>
+            <button className='waves-effect waves-light btn btn red'>Add to Cart</button>
+          </p>
+        </div>
+        <div className='card-reveal '>
+          <span className='card-title grey-text text-darken-4'>
+            {product.name}
+            <i className='material-icons right'>close</i>
+          </span>
+          <p>{product.desciption}</p>
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <div className='section no-pad-bot' id='index-banner'>
       <div className='container'>
         <br />
         <br />
-        <h1 className='header center orange-text'>Home Template</h1>
+        <h1 className='header center orange-text'>Product List</h1>
         <div className='row center'>
           <h5 className='header col s12 light'>
             A modern responsive front-end framework based on Material Design
           </h5>
         </div>
-        <div className='row center'>
-          <Link href='http://materializecss.com/getting-started.html'>
-            <a id='download-button' className='btn-large waves-effect waves-light red'>
-              Get Started
-            </a>
-          </Link>
-        </div>
-        <br />
-        <br />
+        <div className='row productList'>{productList}</div>
       </div>
     </div>
   );
 };
 export async function getStaticProps() {
-  const resp = await "http://localhost:3000/api/products ";
+  const resp = await fetch("http://localhost:3000/api/products");
   const data = await resp.json();
   return {
     props: {
